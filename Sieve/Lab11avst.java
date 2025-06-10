2ffaa5a4b96eebda0dbb01d6fdfdc2d186dd9352
20608 <Dominick.kwasniak@gmail.com>
2025-06-10 12:23:25 -0400

    .

---
+++ Sieve/Lab11avst.java
@@ -0,0 +1,52 @@
+package Sieve;
+// Lab11avst.java
+// The "Sieve of Eratosthenes" Program
+// This is the student, starting version of the Lab11a assignment.
+
+
+import java.util.Scanner;
+
+public class Lab11avst
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        final int MAX = 100;
+        boolean primes[];
+        primes = new boolean[MAX];
+        computePrimes(primes);
+        displayPrimes(primes);
+    }
+
+    public static void computePrimes(boolean primes[]) {
+        System.out.println("\nCOMPUTING PRIME NUMBERS");
+        for (int i = 2; i < primes.length; i++) {
+            primes[i] = true;
+        }
+
+
+        // Student edits start here - 80 point version
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i]) {
+                for (int j = i+i; j < primes.length; j=j+i) {
+                    primes[j] = false;
+
+                }
+            }
+        }
+
+    }
+
+    public static void displayPrimes(boolean primes[])
+    {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
+
+        // Output all values of the list
+        for(int i = 2; i < primes.length; i++) {
+            if (primes[i] == true) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
